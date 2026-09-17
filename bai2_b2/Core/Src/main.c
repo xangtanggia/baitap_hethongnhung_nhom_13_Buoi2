a7198a0c7981d30e3724896e13c14be19b23d385
xangtanggia <112771463+xangtanggia@users.noreply.github.com>
2026-09-17 23:45:07 +0700

    lan3

--- bai2_b2/Core/Src/main.c
+++ bai2_b2/Core/Src/main.c
@@ -1,51 +1,39 @@
-
 #include "main.h"
-
 
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-
-
 
 uint32_t cnt_led1 = 0;
 uint32_t cnt_led2 = 0;
 uint32_t cnt_led3 = 0;
 
-
 int main(void)
 {
-
   
   HAL_Init();
   SystemClock_Config();
   MX_GPIO_Init();
- 
   while (1)
   {
-    
   }
  
 }
-
 
 void HAL_SYSTICK_Callback(void)
 {
     cnt_led1++;
     cnt_led2++;
     cnt_led3++;
-
     if (cnt_led1 >= 5000)
     {
         cnt_led1 = 0;
         HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_0);
     }
-
     if (cnt_led2 >= 500)
     {
         cnt_led2 = 0;
         HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_1);
     }
-
     if (cnt_led3 >= 50)
     {
         cnt_led3 = 0;
@@ -56,8 +44,6 @@ void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-
-  
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
@@ -69,8 +55,6 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-
-  
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
@@ -83,19 +67,12 @@ void SystemClock_Config(void)
     Error_Handler();
   }
 }
-
-
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
- 
   __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
-
-  /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2, GPIO_PIN_RESET);
-
-  /*Configure GPIO pins : PA1 PA2 PA3 */
   GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
@@ -103,11 +80,8 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
 }
-
-
 void Error_Handler(void)
 {
-  
   __disable_irq();
   while (1)
   {
